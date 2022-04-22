a6a1d71a8219913eaab3b3933cd4c7f722a9dd40
kevinhow3117 <69977087+kevinhow3117@users.noreply.github.com>
2022-04-22 13:43:08 +0800

    Add files via upload

---
+++ breathing_LED/breathing.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:49:47 04/16/2022 
+// Design Name: 
+// Module Name:    breathing 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module breathing(clk,rst,sw,led);
+	input clk,rst;
+	input [3:0]sw;
+	output reg led;
+	reg [3:0]count;
+	
+	always@(posedge clk or posedge rst)
+	begin
+		if(rst)
+		begin
+			count<=4'd0;
+			led<=1'b0;
+		end
+		else
+		begin
+			count<=count+1;
+			if(sw>count)
+			begin
+				led<=1'b1;
+				
+			end
+			else
+			begin
+				led<=1'b0;
+			end
+		end
+	end
+endmodule
---
+++ breathing_LED/qq.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:57:44 04/16/2022
+// Design Name:   breathing
+// Module Name:   C:/Users/USER/Desktop/ISE FPGA/pin/breathing_LED/qq.v
+// Project Name:  breathing_LED
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: breathing
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module qq;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg [3:0] sw;
+
+	// Outputs
+	wire led;
+
+	// Instantiate the Unit Under Test (UUT)
+	breathing uut (
+		.clk(clk), 
+		.rst(rst), 
+		.sw(sw), 
+		.led(led)
+	);
+	
+	initial begin
+	clk=0;
+	forever #5 clk=~clk;
+	end
+
+	initial begin
+		rst = 1;
+		sw=4'b0110;
+		#15;
+      rst = 0;
+		sw=4'b0110;
+		#500
+		sw=4'b0100;
+		#500
+		sw=4'b0111;
+	end
+      
+endmodule
+
